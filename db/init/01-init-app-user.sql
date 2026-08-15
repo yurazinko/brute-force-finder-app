90428c3a282dca123ea0dc05665906efe9f35a7f
Yurii Zinko <yurii.zinko@protonmail.com>
2026-08-15 13:06:20 +0200

    Fix rls init script

--- db/init/01-init-app-user.sql
+++ db/init/01-init-app-user.sql
@@ -2,6 +2,8 @@ CREATE USER app_user WITH PASSWORD 'password' NOSUPERUSER CREATEDB;
 
 GRANT ALL PRIVILEGES ON DATABASE brute_force_finder_app_development TO app_user;
 
-\c brute_force_finder_app_development
 GRANT ALL ON SCHEMA public TO app_user;
+GRANT CREATE ON SCHEMA public TO app_user;
+
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO app_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO app_user;
